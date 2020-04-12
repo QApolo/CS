2861facad8b72826f8e9e66c73089ec0e39a25e9
Juan Carlos Garcia Medina <carlos_isc7@outlook.com>
2020-04-12 17:09:49 -0500

    update

--- 03_Conway/game.js
+++ 03_Conway/game.js
@@ -1,7 +1,9 @@
 
-var cellSize = 8;
+var cellSize = 100;
+var cellSizeBordered = cellSize + 1;
+
 class Automaton {
-    constructor(width, height) {
+    constructor(width, height, prob = 0.0) {
 
         var movs =
 			[
@@ -23,38 +25,43 @@ class Automaton {
         this.width = width;
         this.height = height;
 
+        var totalOnes = Math.floor(this.width * prob);
+        for(var i = 0; i < height; i++) {
+            for(var j = 0; j < totalOnes; j++)
+                this.matrix[i][j] = 1;
+        }
+
+        for(var i = 0; i < height; i++) {
+            for(var j = totalOnes; j < width; j++)
+                this.matrix[i][j] = 0;
+            shuffle(this.matrix[i]);
+        }
 
         for(var i = 0; i < width; i++) {
             for(var j = 0; j < height; j++) {
-                var val = Math.floor(Math.random() * 2);
-                //console.log(val);
-                this.matrix[i][j] = val//new Cell(val, i, j);                
-                if(val == 1) {
-                    context.fillStyle = "black";
-                }
-                else {
+                
+                if(this.matrix[i][j] == 1)
+                    setColor();
+                else
                     context.fillStyle = "white";
-                }
-                context.fillRect((cellSize+1)*i, (cellSize+1)*j, cellSize, cellSize);
+                
+                context.fillRect(cellSizeBordered*i, cellSizeBordered*j, cellSize, cellSize);
             }
         }
-       
+
         
     }
-    valid(y, x)
-    {
+    valid(y, x) {
         if(y < 0 || y >= this.height || x < 0 || x >= this.width)
             return false;
         return true;
     }
     countLivingNeighbours(y, x) {
         var cont = 0;
-        for(var i = 0; i < 8; i++)
-        {
+        for(var i = 0; i < 8; i++) {
             var auxY = this.movs[i][0] + y;
             var auxX = this.movs[i][1] + x;
-            if( this.valid(auxY, auxX) )
-            {                              
+            if( this.valid(auxY, auxX) ) {                              
                 if(this.matrix[auxY][auxX] == 1) {
                     cont++;
                 }
@@ -69,54 +76,100 @@ class Automaton {
 
             var states = [];
   
-            for(var i = 0; i < me.width; i++)
-            {
-                for(var j = 0; j < me.height; j++)
-                {                
+            for(var i = 0; i < me.width; i++) {
+                for(var j = 0; j < me.height; j++) {                
                     if(me.matrix[i][j] == 1)
                     {
                         var count = me.countLivingNeighbours(i, j);
-                        if(!(count == 2 || count == 3))
-                        {
+                        if(!(count == 2 || count == 3)) {
                             states.push([i, j, 0]);
                         }
                     }
-                    else if(me.matrix[i][j] == 0)
-                    {
+                    else if(me.matrix[i][j] == 0) {
                         var count = me.countLivingNeighbours(i, j);
-                        if( count == 3 )
-                        {
+                        if( count == 3 ) {
                             states.push([i, j, 1]);
                         }
                     }
                 }
             }
-            for(var i = 0; i < states.length; i++)
-            {        
+            for(var i = 0; i < states.length; i++) {        
                 me.matrix[states[i][0]][states[i][1]] = states[i][2];
                 if(states[i][2] == 1)
-                    context.fillStyle = "black";
+                    setColor();
                 else
                     context.fillStyle = "white";
-                context.fillRect((cellSize+1)*states[i][0], (cellSize+1)*states[i][1], cellSize, cellSize);
+                context.fillRect(cellSizeBordered*states[i][0], cellSizeBordered*states[i][1], cellSize, cellSize);
             }
             time--;
-            if(time >= 0)
-            {
-                setTimeout(drawGrid, 100, me);
+            if(time >= 0) {
+                setTimeout(drawGrid, 50, me);
             }       
         };
         drawGrid(me);
     }
+    setLivingCell(x, y) {
+        
+        if(!this.valid(y, x))
+            return
+        this.matrix[y][x] = 1;
+    }
 }
 
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    //return a;
+}
 
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+function setColor()
+{
+    var colorChooser = document.getElementById("colorChooser");
+    context.fillStyle = "#"+colorChooser.value;
+}
 var myCanvas = document.querySelector("canvas");
+myCanvas.width = 3 * cellSizeBordered;
+myCanvas.height = 3 * cellSizeBordered;
 var context = myCanvas.getContext("2d");
-context.fillStyle = "black";
+setColor();
 
-automaton = new Automaton(100, 100);
-automaton.routine(10000);
+startButton = document.getElementById("start");
+startButton.onclick = function()
+{
+    automaton.routine(10000);
+};
+
+
+var automaton = new Automaton(myCanvas.width, myCanvas.height, 0.5);
+
+myCanvas.onmousemove = function(evt) {
+    var mousePos = getMousePos(myCanvas, evt);
+    
+    var tmp = context.fillStyle;
+    setColor();
+
+    var currX = Math.floor(Math.floor(mousePos.x / cellSizeBordered) * cellSizeBordered);
+    var currY = Math.floor(Math.floor(mousePos.y / cellSizeBordered) * cellSizeBordered);
+    automaton.setLivingCell(Math.floor(currX / cellSizeBordered), Math.floor(currY/cellSizeBordered));
+    context.fillRect(currX, currY, cellSize, cellSize);
+    context.fillStyle = tmp;
+};
+
+
+
 
 
 
